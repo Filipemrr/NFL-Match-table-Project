d64728a4454ffbcc8dd7264a75e8226dfc715af5
filipemmr <112185872+Filipemrr@users.noreply.github.com>
2023-04-07 21:47:44 -0300

    Almost Done

--- main.js
+++ main.js
@@ -1,35 +1,32 @@
 function createGame(player1, hour, player2) {
   return `
-      <li>
-          <img src="./assets/icon-${player1}.svg" alt="brazil" />
-          <strong>${hour}</strong>
-          <img src="./assets/icon-${player2}.svg" alt="Bandeira da Sérvia" />
-      </li>
+    <li>
+      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+      <strong>${hour}</strong>
+      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+    </li>
   `
 }
 
+let delay = -0.4
 function createCard(date, day, games) {
+  delay = delay + 0.4
   return `
-  <div class="card">
-      <h2>${date}<span>${day}</span></h2>
+    <div class="card" style="animation-delay: ${delay}s">
+      <h2>${date} <span>${day}</span></h2>
       <ul>
         ${games}
       </ul>
     </div>
   `
 }
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="Logo da NLW" />
-  </header>
-  <main id="cards">
-    ${createCard(
-      "24/11",
-      "quinta",
-      createGame("brazil", "16:00", "serbia") +
-        createGame("Portugal", "16:00", "Uruguay")
-    )} 
-    ${createCard("28/11", "segunda", createGame("swiss", "13:00", "brazil"))} 
-    ${createCard("02/12", "sexta", createGame("brazil", "16:00", "camaroon"))} 
-  </main>
-`
+
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("swiss", "13:00", "brazil") +
+      createGame("portugal", "16:00", "uruguay")
+  ) +
+  createCard("02/12", "sexta", createGame("brazil", "16:00", "camaroon"))
